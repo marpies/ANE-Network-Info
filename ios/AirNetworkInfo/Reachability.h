46f1b3bd9d249f290f9517c2ebfdc955edba139e
Marcel Piešťanský <marcel@piestansky.cz>
2018-12-13 17:52:49 +0100

    Updated Reachability

--- ios/AirNetworkInfo/Reachability.h
+++ ios/AirNetworkInfo/Reachability.h
@@ -1,7 +1,7 @@
 /*
  Copyright (C) 2016 Apple Inc. All Rights Reserved.
  See LICENSE.txt for this sample’s licensing information
-
+ 
  Abstract:
  Basic demonstration of how to use the SystemConfiguration Reachablity APIs.
  */
@@ -25,38 +25,40 @@ extern NSString *kReachabilityChangedNotification;
 
 
 @interface Reachability : NSObject
-
-/*!
- * Use to check the reachability of a given host name.
- */
+    
+    /*!
+     * Use to check the reachability of a given host name.
+     */
 + (instancetype)reachabilityWithHostName:(NSString *)hostName;
-
-/*!
- * Use to check the reachability of a given IP address.
- */
+    
+    /*!
+     * Use to check the reachability of a given IP address.
+     */
 + (instancetype)reachabilityWithAddress:(const struct sockaddr *)hostAddress;
-
-/*!
- * Checks whether the default route is available. Should be used by applications that do not connect to a particular host.
- */
+    
+    /*!
+     * Checks whether the default route is available. Should be used by applications that do not connect to a particular host.
+     */
 + (instancetype)reachabilityForInternetConnection;
-
-
+    
+    
 #pragma mark reachabilityForLocalWiFi
-//reachabilityForLocalWiFi has been removed from the sample.  See ReadMe.md for more information.
-//+ (instancetype)reachabilityForLocalWiFi;
-
-/*!
- * Start listening for reachability notifications on the current run loop.
- */
+    //reachabilityForLocalWiFi has been removed from the sample.  See ReadMe.md for more information.
+    //+ (instancetype)reachabilityForLocalWiFi;
+    
+    /*!
+     * Start listening for reachability notifications on the current run loop.
+     */
 - (BOOL)startNotifier;
 - (void)stopNotifier;
-
+    
 - (NetworkStatus)currentReachabilityStatus;
-
-/*!
- * WWAN may be available, but not active until a connection has been established. WiFi may require a connection for VPN on Demand.
- */
+    
+    /*!
+     * WWAN may be available, but not active until a connection has been established. WiFi may require a connection for VPN on Demand.
+     */
 - (BOOL)connectionRequired;
+    
+    @end
 
-@end
+
